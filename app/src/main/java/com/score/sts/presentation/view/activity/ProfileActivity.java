843414e5e0bfe60020ac79366c5523a3683600d4
victory.turpin <vturp@yahoo.com>
2016-05-03 21:40:25 -0400

    refactored landing and profile pages. finalized base layout for profile page

--- app/src/main/java/com/score/sts/presentation/view/activity/ProfileActivity.java
+++ app/src/main/java/com/score/sts/presentation/view/activity/ProfileActivity.java
@@ -9,9 +9,11 @@ import android.os.Bundle;
 import com.score.sts.R;
 import com.score.sts.presentation.view.fragment.BioProfileFragment;
 import com.score.sts.presentation.view.fragment.ContactsProfileFragment;
+import com.score.sts.presentation.view.fragment.MessageChatProfileFragment;
 import com.score.sts.presentation.view.fragment.MusicProfileFragment;
 import com.score.sts.presentation.view.fragment.RegisterWorkProfileFragment;
-import com.score.sts.presentation.view.fragment.TbdProfileFragment;
+import com.score.sts.presentation.view.fragment.ProfileFragment;
+import com.score.sts.presentation.view.fragment.VideosProfileFragment;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -31,18 +33,22 @@ public class ProfileActivity extends AppCompatActivity {
         FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction ft = fm.beginTransaction();
 
-        TbdProfileFragment tbdProfileFragment = new TbdProfileFragment();
+        ProfileFragment profileFragment = new ProfileFragment();
         MusicProfileFragment musicProfileFragment = new MusicProfileFragment();
         ContactsProfileFragment contactsProfileFragment = new ContactsProfileFragment();
         RegisterWorkProfileFragment registerWorkProfileFragment = new RegisterWorkProfileFragment();
         BioProfileFragment bioProfileFragment = new BioProfileFragment();
+        VideosProfileFragment videosProfileFragment = new VideosProfileFragment();
+        MessageChatProfileFragment messageChatProfileFragment = new MessageChatProfileFragment();
 
         if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
-            ft.add(R.id.fl_profile_tbd_fragment, tbdProfileFragment);
+            ft.add(R.id.fl_profile_fragment, profileFragment);
             ft.add(R.id.fl_profile_music_fragment, musicProfileFragment);
             ft.add(R.id.fl_profile_contacts_fragment, contactsProfileFragment);
             ft.add(R.id.fl_profile_register_work_fragment, registerWorkProfileFragment);
             ft.add(R.id.fl_profile_bio_fragment, bioProfileFragment);
+            ft.add(R.id.fl_profile_videos_fragment, videosProfileFragment);
+            ft.add(R.id.fl_profile_msg_chat_facetime, messageChatProfileFragment);
             ft.commit();
         }
     }
--- app/src/main/java/com/score/sts/presentation/view/fragment/ProfileFragment.java
+++ app/src/main/java/com/score/sts/presentation/view/fragment/ProfileFragment.java
@@ -1,45 +1,30 @@
 package com.score.sts.presentation.view.fragment;
 
-import android.content.res.Configuration;
+
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 
 import com.score.sts.R;
 
-import butterknife.ButterKnife;
-
 /**
- * Created by Who Dat on 2/9/2016.
+ * A simple {@link Fragment} subclass.
  */
 public class ProfileFragment extends Fragment {
 
-    public static final String TAG = ProfileFragment.class.getSimpleName();
 
-    @Nullable
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View profileView = inflater.inflate(R.layout.profile_fragment, container, false);
-        ButterKnife.bind(this, getActivity());
-        setPortraitOrientationMargins(profileView);
-        return profileView;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.profile_fragment, container, false);
     }
 
-    /**
-     * The star is not aligned correctly on portrait mode. this method
-     * dynamically sets the margin for the star icon
-     */
-    private void setPortraitOrientationMargins(View view){
-        if(getActivity().getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
-            ImageView imageView = (ImageView) view.findViewById(R.id.ivSignUpStar);
-            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-            layoutParams.setMargins(35,10,0,0);
-            imageView.setLayoutParams(layoutParams);
-        }
-    }
 }
